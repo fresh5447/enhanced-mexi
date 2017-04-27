2cbd6b63533606a97d74298ffadebbb5ae7c248d
fresh5447 <doug@kosmojo.com>
2017-04-27 17:44:32 -0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,28 @@
+export default {
+	title: "Juans Burrito Stop",
+	phone: "(404) 788-4545",
+	menuItems: [
+		{ item: "Pork Tacos", spiceLevel: 5 },
+		{ item: "Enchilada", spiceLevel: 7 },
+		{ item: "Tamale", spiceLevel: 3 },
+		{ item: "Chalupa & Enchilada", spiceLevel: 8 },
+		{ item: "Chile Relleno", spiceLevel: 2 },
+		{ item: "Cheese Crisp", spiceLevel: 2 },
+		{ item: "Inferno Cheese Crisp", spiceLevel: 9 },
+		{ item: "Sopapillas", spiceLevel: 0 },
+		{ item: "Tortilla Soup", spiceLevel: 4 },
+		{ item: "Death Taco", spiceLevel: 10 },
+		{ item: "Beans and Rice", spiceLevel: 1 },
+		{ item: "Piscina de la Muerte Chimichanga", spiceLevel: 3 },
+		{ item: "Quesadilla Platter", spiceLevel: 4 },
+		{ item: "Carnitas Diablo", spiceLevel: 7 },
+		{ item: "Pozole", spiceLevel: 2 },
+		{ item: "All You Can Eat Churros", spiceLevel: 1 },
+		{ item: "Adobo", spiceLevel: 6 },
+		{ item: "Elote", spiceLevel: 2 },
+		{ item: "Huevos Rancheros", spiceLevel: 5 },
+		{ item: "Arroz con Pollo", spiceLevel: 3 },
+		{ item: "Sopaipilla", spiceLevel: 2 },
+
+	]
+}
