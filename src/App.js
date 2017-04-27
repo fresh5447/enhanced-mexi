3c724a133bb462bb5dbb33d8523cd1e3006941b7
fresh5447 <doug@kosmojo.com>
2017-04-27 17:45:20 -0600

    prettier

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import restaurantData from './data';
-
 
 class MenuSelect extends Component {
   state = {
     spiceThresh: 10,
+  };
+  textChange(event) {
+    this.setState({spiceThresh: Number(event.target.value)});
   }
-	textChange(event){
-		this.setState({spiceThresh: Number(event.target.value)});
-	}
-	render(){
+  render() {
     console.log(restaurantData);
-		return(
-			<div className="container">
-				<h1>Your goal here is to:</h1>
-				<ul>
-				<li>1. Develop a restaurant menu interface</li>
-				<li>2. Allow users to enter in a spice level
-					 to a text field</li>
-				<li>3. Use input data to filter the given menu
-					 items</li>
-					<li>4. You must place your input interface within a separate
-					 "dumb" react component and call it here</li>
-					<li>NOTE: If no input is entered, all menu items should display</li>
-				</ul>
-				<img src="http://vignette2.wikia.nocookie.net/bookoflife/images/7/73/All_you_can_eat_churros.PNG/revision/latest?cb=20140910221451"/>
-			</div>
-		)
-	}
+    return (
+      <div className="container">
+        <h1>Your goal here is to:</h1>
+        <ul>
+          <li>1. Develop a restaurant menu interface</li>
+          <li>
+            2. Allow users to enter in a spice level
+            to a text field
+          </li>
+          <li>
+            3. Use input data to filter the given menu
+            items
+          </li>
+          <li>
+            4. You must place your input interface within a separate
+            "dumb" react component and call it here
+          </li>
+          <li>NOTE: If no input is entered, all menu items should display</li>
+        </ul>
+        <img src="http://vignette2.wikia.nocookie.net/bookoflife/images/7/73/All_you_can_eat_churros.PNG/revision/latest?cb=20140910221451" />
+      </div>
+    );
+  }
 }
-
 
 export default MenuSelect;
